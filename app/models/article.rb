8ac6db60b281be67b773fd11d677497243d764e6
yaniiliita <gisel_cupul93@hotmail.com>
2015-07-27 09:48:57 -0500

    Initial commit

---
+++ app/models/article.rb
@@ -0,0 +1,22 @@
+class Article < ActiveRecord::Base
+	#La tabla => articles
+	#Campos => article.title() => 'título del artículo'
+	#Escribir métodos
+
+	belongs_to :user
+	validates :title, presence: true, uniqueness: true
+	validates :body, presence: true, length: {minimum: 20}
+	before_save :set_visits_count
+
+	def update_visits_count
+		
+		self.update(visits_count: self.visits_count + 1)
+	end
+
+
+	private
+
+	def set_visits_count
+		self.visits_count ||= 0
+	end
+end
